f4517da3d3230ea80fbd1b00e2c60dc6587fa040
Adam O'Connor <adamoconnor32@gmail.com>
2022-10-16 15:55:02 -0500

    Fixed Defender Momentum

--- Source_Code/defender_1.py
+++ Source_Code/defender_1.py
@@ -12,9 +12,15 @@ class defender(entity):
 
         self.rect.y += self.velocity
         self.rect.y += self.velocity
+
+        #Running Back is to the Right
         if running_back.get_object_rect().x - self.rect.x > 2:
-            self.x_momentum += self.velocity/2
+            if self.x_momentum < self.velocity:
+                self.x_momentum += self.velocity/10
+
+        #Running Back is to the Left
         elif running_back.get_object_rect().x - self.rect.x < -2:
-            self.x_momentum -= self.velocity/2
-        
+            if (-1)*self.x_momentum < self.velocity:
+                self.x_momentum -= self.velocity/10
+    
         self.rect.x += self.x_momentum
