7336738729d91ae5ba5502b4d60cf5437a0e04af
Joseph-Stolfa <joey.stolfa@okstate.edu>
2022-10-16 00:41:14 -0500

    added destroy method to bullet

--- Source_Code/bullet.py
+++ Source_Code/bullet.py
@@ -1,1 +1,11 @@
 import pygame
+from entity import entity
+
+class bullet(entity):
+	def __init__(self,sprite_path,pos,velocity,screen_size):
+		super().__init__(sprite_path,pos,velocity)
+		self.screen_size = screen_size
+
+	def destroy(self):
+		if self.rect.y <= -50 or self.rect.y >= self.screen_size[1] + 50: 
+			self.kill()
