fdac2568284f61b970a4d6ce1a15c54d74388a5f
Adam O'Connor <adamoconnor32@gmail.com>
2022-10-16 15:34:35 -0500

    Added Momentum to defenders.

--- Source_Code/defender_1.py
+++ Source_Code/defender_1.py
@@ -4,11 +4,17 @@ from running_back import running_back
 
 class defender(entity):
 
+    def __init__(self,sprite_path,pos,velocity):
+        super().__init__(sprite_path, pos, velocity)
+        self.x_momentum = 0
+
     def update(self, running_back):
 
         self.rect.y += self.velocity
         self.rect.y += self.velocity
         if running_back.get_object_rect().x - self.rect.x > 2:
-            self.rect.x += self.velocity
+            self.x_momentum += self.velocity/2
         elif running_back.get_object_rect().x - self.rect.x < -2:
-            self.rect.x -= self.velocity
+            self.x_momentum -= self.velocity/2
+        
+        self.rect.x += self.x_momentum
