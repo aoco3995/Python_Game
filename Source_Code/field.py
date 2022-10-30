850ec8c693a20fb804fbfbad74d1a35b396e2b01
Adam O'Connor <adamoconnor32@gmail.com>
2022-10-30 11:59:43 -0500

    Fullscreen field opening.

--- Source_Code/field.py
+++ Source_Code/field.py
@@ -7,20 +7,5 @@ class field(entity):
         super().__init__(sprite_path, pos, velocity)
         self.rect = self.image.get_rect(bottomleft = pos)
 
-    def update(self, running_back):
-
-        self.rect.y += self.velocity
-        self.rect.y += self.velocity
-
-        if self.rect.y < running_back.get_object_rect().y:
-            #Running Back is to the Right
-            if running_back.get_object_rect().x - self.rect.x > 2:
-                if self.x_momentum < self.velocity:
-                    self.x_momentum += 0.1
-
-            #Running Back is to the Left
-            elif running_back.get_object_rect().x - self.rect.x < -2:
-                if (-1)*self.x_momentum < self.velocity:
-                    self.x_momentum -= 0.1
-    
-        self.rect.x += self.x_momentum
+    def update(self):
+        pass
