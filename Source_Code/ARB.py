a68d1acbbebca3a9bed63c88bb3ae62243823eba
Joseph-Stolfa <joey.stolfa@okstate.edu>
2022-10-31 22:46:13 -0500

    controls

--- Source_Code/ARB.py
+++ Source_Code/ARB.py
@@ -13,12 +13,34 @@ class ARB(entity):
 		self.animation_step = 0
 		self.animation_action = 0 #temp
 		self.last_update = pygame.time.get_ticks()
-		self.animation_cooldown = 60
+		self.animation_cooldown = 40
+		
+
+
+	def get_input(self):
+		self.keys = pygame.key.get_pressed()
+		if self.keys[pygame.K_a]:
+			self.animation_action = 0
+		if self.keys[pygame.K_s]:
+			self.animation_action = 6
+		if self.keys[pygame.K_d]:
+			self.animation_action = 4
+		if self.keys[pygame.K_w]:
+			self.animation_action = 2
+		if self.keys[pygame.K_w] and self.keys[pygame.K_a]:
+			self.animation_action = 1
+		if self.keys[pygame.K_w] and self.keys[pygame.K_d]:
+			self.animation_action = 3
+		if self.keys[pygame.K_s] and self.keys[pygame.K_a]:
+			self.animation_action = 7
+		if self.keys[pygame.K_s] and self.keys[pygame.K_d]:
+			self.animation_action = 5
 
 	def animation(self,field, screen, running_back):
 		current_time = pygame.time.get_ticks()
+		screen.blit(self.image, running_back.get_abs_pos(field), ((112.5*self.animation_step),(112.5*self.animation_action),(112.5),(112.5)))
 		if current_time - self.last_update >= self.animation_cooldown:
-			screen.blit(self.image, running_back.get_abs_pos(field), ((112.5*self.animation_step),(0),(112.5+112.5*self.animation_step),(112.5)))
+			
 			if self.animation_step < 7:
 				self.animation_step += 1
 			else:
@@ -26,4 +48,5 @@ class ARB(entity):
 			self.last_update = pygame.time.get_ticks()
 
 	def update(self, field, screen, running_back):
+		self.get_input()
 		self.animation(field, screen, running_back)
